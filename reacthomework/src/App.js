a373fc683900eb03b4cf3a3f0b47e0e8b8f8b946
Javonte Tuppince <taetupp47@gmail.com>
2021-02-10 03:42:15 -0500

    got page to render on page

--- reacthomework/src/App.js
+++ reacthomework/src/App.js
@@ -1,40 +1,11 @@
 import './App.css';
-import React, { Component } from "react";
-import Header from "./compoents/Header/header";
-import Search from "./compoents/Search/search";
-import Wrapper from "./compoents/Wrapper/wrapper";
-import Table from "./compoents/Table/table";
-import employees from "./employee.json";
-class App extends Component {
-  state = {
-    employees
-  };
+import React from "react";
+import Index from "./pages/index"
 
-  removeEmployee = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    const employees = this.state.employees.filter(employee => employee.id !== id);
-    // Set this.state.friends equal to the new friends array
-    this.setState({ employees });
-  };
-
-  render(){
+function App() {
   return (
-    <Wrapper>
-      <Header>Employee List</Header>
-      <Search/>
-      {this.state.employees.map(employee => (
-        <Table
-          removeEmployee={this.removeEmployee}
-          id={employee.id}
-          key={employee.id}
-          name={employee.name}
-          image={employee.image}
-          occupation={employee.occupation}
-          location={employee.location}
-        />
-      ))}
-    </Wrapper>
-  )};
+  <Index/>
+  );
 }
 
 export default App;
